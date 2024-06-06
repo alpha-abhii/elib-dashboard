3c2738aedb75906d913fcd029233e38c85e75a5f
alpha-abhii <alpha20abhi@gmail.com>
2024-06-07 02:37:05 +0530

    add create book endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,3 +1,4 @@
+import useTokenStore from "@/store";
 import axios from "axios";
 
 const api = axios.create({
@@ -9,6 +10,15 @@ const api = axios.create({
     }
 })
 
+api.interceptors.request.use((config) => {
+    const token = useTokenStore.getState().token;
+
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+})
+
 export const login = async (data: {email: string; password: string}) => {
     return api.post("/api/v1/users/login",data);
 }
@@ -18,4 +28,12 @@ export const register = async (data: {name: string; email: string; password: str
 
 export const getbooks = async () => {
     return api.get("/api/v1/books/list")
+}
+
+export const createBook = async (data: FormData) => {
+    return api.post("/api/v1/books/create",data,{
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        }
+    })
 }
